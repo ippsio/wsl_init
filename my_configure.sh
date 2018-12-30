360c60e9fa0a42de36bccbce1ff133e9771f3376
ippsio <ippei.shioiri@gmail.com>
2018-12-30 12:09:25 +0900

    commit

--- my_configure.sh
+++ my_configure.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+cp ./.profile ~/.profile
+source ~/.profile
+
 umask 022
-unsetopt BG_NICE
+#unsetopt BG_NICE
 
 install() {
   echo "INSTALL '$1'."
   installed=$(dpkg -l $1; echo $?)
-  if [ "${installed}" == "0" ]; then
+  if [ "${installed}" != "0" ]; then
     sudo apt-get install -y $1
   else
     echo "SKIP. ALREADY INSTALLED"
@@ -16,10 +20,20 @@ install git
 install awk
 install vim
 install zsh
+
+# docker
+install docker.io
+sudo cgroupfs-mount
+sudo usermod -aG docker $USER
+sudo service docker start
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 cp ./.zshrc.org ~/.zshrc
 cp ./.zshenv.org ~/.zshenv
 cp ./.zshenv.org ~/.zshenv
-cp ./.profile ~/.profile
 
 # zplug
 mkdir -p ~/.zsh
